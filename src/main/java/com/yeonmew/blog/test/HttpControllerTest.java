324bb37a1947909cfb7394f764c8e81f5c931eb6
ymew <yeon3191225@gmail.com>
2022-12-30 00:16:27 +0900

    http요청 실습2(완)

--- src/main/java/com/yeonmew/blog/test/HttpControllerTest.java
+++ src/main/java/com/yeonmew/blog/test/HttpControllerTest.java
@@ -13,25 +13,25 @@ public class HttpControllerTest {
     // http://localhost:8080/http/get (select)
     // 인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다.
     @GetMapping("/http/get")
-    public String getTest(Member m) {
+    public String getTest(Member m) { //id=1&username=ssar&password=1234&email=mew@mail.com //MessageConverter (스프링부트)
         return "get 요청:" + m.getId() + "," + m.getUsername() + "," + m.getPassword()+","+m.getEmail();
     }
 
     // http://localhost:8080/http/post (insert)
-    @PostMapping("/http/post")
-    public String postTest(String text) {
-        return "post 요청:" + text;
+    @PostMapping("/http/post") //text/plain
+    public String postTest(@RequestBody Member m) { //MessageConverter (스프링부트)
+        return "post 요청:" + m.getId() + "," + m.getUsername() + "," + m.getPassword()+","+m.getEmail();
     }
 
     // http://localhost:8080/http/put (update)
     @PutMapping("/http/put")
-    public String putTest() {
-        return "put 요청";
+    public String putTest(@RequestBody Member m) {
+        return "put 요청:" + m.getId() + "," + m.getUsername() + "," + m.getPassword() + "," + m.getEmail();
     }
 
     // http://localhost:8080/http/delete (delete)
     @DeleteMapping("/http/delete")
-    public String deleteTest() {
-        return "delete 요청";
+    public String deleteTest(@RequestBody Member m) {
+        return "delete 요청:" + m.getId() + "," + m.getUsername() + "," + m.getPassword() + "," + m.getEmail();
     }
 }
