628119ad6edf06b27d06e45af5d0225c8ea2b948
ymew <yeon3191225@gmail.com>
2022-12-29 23:45:51 +0900

    http 실습2

--- src/main/java/com/yeonmew/blog/test/HttpControllerTest.java
+++ src/main/java/com/yeonmew/blog/test/HttpControllerTest.java
@@ -9,28 +9,29 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HttpControllerTest {
 
+
     // http://localhost:8080/http/get (select)
     // 인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다.
     @GetMapping("/http/get")
-    public String getTest(){
-        return "get 요청";
+    public String getTest(Member m) {
+        return "get 요청:" + m.getId() + "," + m.getUsername() + "," + m.getPassword()+","+m.getEmail();
     }
 
     // http://localhost:8080/http/post (insert)
     @PostMapping("/http/post")
-    public String postTest(){
-        return "post 요청";
+    public String postTest(String text) {
+        return "post 요청:" + text;
     }
 
     // http://localhost:8080/http/put (update)
     @PutMapping("/http/put")
-    public String putTest(){
+    public String putTest() {
         return "put 요청";
     }
 
     // http://localhost:8080/http/delete (delete)
     @DeleteMapping("/http/delete")
-    public String deleteTest(){
+    public String deleteTest() {
         return "delete 요청";
     }
 }
